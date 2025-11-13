d45044b32eea34decdfbf0371914157be9dfa981
Contre <lucascontre95@gmail.com>
2025-11-13 21:01:19 +0100

    Update 13-11-25

--- dotfiles/nixos/programs/slicer.nix
+++ dotfiles/nixos/programs/slicer.nix
@@ -28,19 +28,19 @@
           #   desktopName = "Orca Slicer";
           #   terminal = false;
           # })
-          # (pkgs.makeDesktopItem {
-          #   name = "bambu-studio";
-          #   exec = "${(pkgs.writeScriptBin "bambu-studio-wrapper" ''
-          #     #!/bin/sh
-          #     export __GLX_VENDOR_LIBRARY_NAME=mesa
-          #     export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json
-          #     exec ${pkgs.bambu-studio}/bin/bambu-studio "$@"
-          #   '')}/bin/bambu-studio-wrapper";
-          #   icon = "print"; # Replace with the actual icon name or path if available
-          #   comment = "3D Printing Slicer";
-          #   desktopName = "Bambu Studio";
-          #   terminal = false;
-          # })
+          (pkgs.makeDesktopItem {
+            name = "bambu-studio";
+            exec = "${(pkgs.writeScriptBin "bambu-studio-wrapper" ''
+              #!/bin/sh
+              export __GLX_VENDOR_LIBRARY_NAME=mesa
+              export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json
+              exec ${pkgs.bambu-studio}/bin/bambu-studio "$@"
+            '')}/bin/bambu-studio-wrapper";
+            icon = "print"; # Replace with the actual icon name or path if available
+            comment = "3D Printing Slicer";
+            desktopName = "Bambu Studio";
+            terminal = false;
+          })
         ];
       }
     else
